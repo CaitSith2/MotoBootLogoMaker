cfb1f357986ed3348aac06a7eb4b277cc252590a
CaitSith2 <d_good@caitsith2.com>
2015-10-21 15:19:17 -0700

    Revised the device listing.  Since all 3 generations of Moto G are the same, screen size and logo.bin partition size wise,  and All 2 generations are the same screen size wise and logo.bin partion size wise, they have been merged together.

--- Moto_Logo/Properties/AssemblyInfo.cs
+++ Moto_Logo/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.1020.0513")]
-[assembly: AssemblyFileVersion("2.3.1020.0513")]
+[assembly: AssemblyVersion("2.4.1021.2217")]
+[assembly: AssemblyFileVersion("2.4.1021.2217")]
